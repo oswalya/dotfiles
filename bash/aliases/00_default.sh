31dc23265b8bbca5e9698fff20b81390b4701aec
Björn Jessen-Noak <jessen-noak@mediamarktsaturn.com>
2019-06-07 11:12:21 +0200

    add aliases and prompt

---
+++ bash/aliases/00_default.sh
@@ -0,0 +1,65 @@
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls -F --color=auto'
+fi
+
+if [ -e /usr/bin/ag ] ; then
+    alias ag="ag --color-match='1;31' --color-line='1;32' --color-path='35'"
+fi
+
+# aliases
+alias watch='watch '
+alias ll='ls -l'
+alias la='ls -la'
+alias lla='ls -la'
+alias cp='cp -vr'
+alias mv='mv -v'
+alias rm='rm -v'
+alias du='du -ch'
+alias grep="grep --color=auto"
+alias clip='xclip -selection clipboard'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias hg='history | grep -i'
+alias diff="diff -u"
+alias utop='top -u "$USER"'
+alias g='grep'
+alias mkdir='mkdir -p'
+alias df='df -h'
+alias ducks='du -cksh * | sort -rh | head -11' # Lists folders and files sizes in the current folder
+alias abs='readlink -f' #shows the absolute path
+alias sx="screen -x"
+alias cx="chmod +x"
+alias il='ip addr | grep inet | sed -e "s#\s*inet \([0-9.]*\).*\ \([a-z0-9]*\)#\2 \1#g"'
+alias txt='vim -c "set wrap linebreak nolist nonumber noshowmode noruler laststatus=0 noshowcmd"' # for reading texts
+alias cl='crontab -l'
+alias ce='crontab -e'
+alias gu='git pull && git submodule update --init --recursive'
+alias g1="git diff HEAD~1"
+alias -- -='cd -'
+alias gc="gcloud"
+alias kc="kubectl"
+
+#colourise
+if [ -e /usr/bin/grc ]
+then
+	alias grc='grc -es'
+	alias ping='grc ping'
+	alias traceroute='grc /usr/sbin/traceroute'
+	alias netstat='grc netstat'
+	alias diffc='grc diff -u'
+	alias make='grc make'
+	function log () {
+		grc cat "$1" | less
+	}
+fi
+
+#use htop instead of top
+if [ -e /usr/bin/htop ]
+then
+	alias top='htop'
+fi
+
+
---
+++ bash/prompt/fancy.sh
@@ -0,0 +1,76 @@
+if [ "$(whoami)" == "root" ] ; then
+	ROOT=" - \[\033[01;91m\]root!\[\033[00m\]"
+else
+	ROOT=""
+fi
+
+#SHORTEN PWD
+_PS1 ()
+{
+	ret="$?"
+	local PRE= NAME="$1" LENGTH="$2";
+	[[ "$NAME" != "${NAME#$HOME/}" || -z "${NAME#$HOME}" ]] &&
+	PRE+='~' NAME="${NAME#$HOME}" LENGTH=$[LENGTH-1];
+	((${#NAME}>$LENGTH)) && NAME="/...${NAME:$[${#NAME}-LENGTH+4]}";
+	echo "$PRE$NAME"
+	return $ret #save the returnvalue
+}
+
+FQDN="$(hostname -f)"
+
+if which mawk &> /dev/null ; then
+    AWK=mawk
+else
+    AWK=gawk\ --non-decimal-data
+fi
+
+COLOR_CODES="$($AWK '
+
+function brightness (r, g, b)
+{
+    return int(sqrt(r * r * 0.299 + g * g * 0.587 + b * b * 0.114))
+}
+
+function lighten (n) {
+    return n + (255 - n) * 0.1
+}
+
+BEGIN{
+n = "'"$(sha512sum <<< "$FQDN" | cut -d" " -f1)"'"
+position = 1
+threshold = 100
+while ((brightness(r1, b1, g1) < threshold || brightness(r2, b2, g2) < threshold) && position < 120) {
+	r1 = lighten(int("0x" substr(n, position, 2)))
+	r2 = lighten(int("0x" substr(n, position + 2, 2)))
+	g1 = lighten(int("0x" substr(n, position + 4, 2)))
+	g2 = lighten(int("0x" substr(n, position + 6, 2)))
+	b1 = lighten(int("0x" substr(n, position + 8, 2)))
+	b2 = lighten(int("0x" substr(n, position + 10, 2)))
+	position = position + 2
+}
+printf "%i;%i;%i\n", r1, g1, b1
+printf "%i;%i;%i", r2, g2, b2
+}')"
+
+COLOR_ARRAY=(${COLOR_CODES//\n/ })
+
+#Command Number
+#CMDNR="\!"
+#User
+U="\[\033[38;2;${COLOR_ARRAY[0]}m\]\u\[\033[00m\]"
+
+#Host
+H="\[\033[38;2;${COLOR_ARRAY[1]}m\]$(hostname -f)\[\033[00m\]"
+
+#Directory
+DIR="\[\033[01;34m\]"'$(_PS1 "$PWD" 50)'"\[\033[00m\]"
+#Date
+DATE="\e[1m\t \d\[\033[00m\]"
+#Current tty
+TTY="\l"
+#Return code
+RETURN="\$(ret=\$?; if [[ \$ret = 0 ]];then echo \"\[\033[01;32m\]✓\";else echo \"\[\033[01;31m\]\$ret\";fi)\[\033[00m\]"
+#
+
+#the actual prompt with a colorised return code
+PS1="$DATE - $DIR$ROOT\n$U@$H $RETURN \$ " #schnell
