a394c96db3dafb4c4bbc73111cd30cefefa08a13
Björn Jessen-Noak <jessen-noak@mediamarktsaturn.com>
2022-03-29 09:21:59 +0200

    update apps and completion

--- bash/completions/00_default.sh
+++ bash/completions/00_default.sh
@@ -5,3 +5,11 @@ fi
 if [ -f $(brew --prefix)/etc/bash_completion ]; then 
     . $(brew --prefix)/etc/bash_completion
 fi
+
+if [ -f $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc ]; then
+    . $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
+fi
+
+if [ -f $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc ]; then
+    . $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+fi
